361243f353418c50f55f8c2397b1ef8919f8037b
Navdeep_Mishra <navdeepmishra82734@gmail.com>
2021-10-21 22:03:43 +0530

    initial commit

---
+++ src/components/Mainbody.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Container from '@mui/material/Container'
+import Box from '@mui/material/Box';
+import SearchBox from './sub/SearchBox';
+import Grid from '@mui/material/Grid';
+const MainBody = () =>
+{
+	return(
+		<Container maxWidth="xl">
+			{/* <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} /> */}
+			<Grid container spacing={1} sx={{justifyContent: 'center'}}>
+				<Grid item xs={8}>
+					<SearchBox/>
+				</Grid>
+				
+			</Grid>
+		</Container>
+	)
+}
+
+export default MainBody
---
+++ src/components/sub/SearchBox.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Paper from '@mui/material/Paper'
+import search from '../../imgs/search/search.svg'
+export default function SearchBox() {
+	return (
+		<div>
+			
+				<img src={search} style={{width: '200px'}} />
+			
+		</div>
+	)
+}
