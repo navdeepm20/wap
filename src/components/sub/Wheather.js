3cce11449981d76b8485b031fd1a35c534072bb4
Navdeep_Mishra <navdeepmishra82734@gmail.com>
2021-10-25 22:55:03 +0530

    added weather icons

--- src/components/sub/Wheather.js
+++ src/components/sub/Wheather.js
@@ -1,7 +1,10 @@
 import React,{useContext} from 'react'
-import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import { GetDataContext } from '../../contexts/GetData';
+import Icons from '../../images';
+
+
 function Wheather() {
 	const wdata = useContext(GetDataContext)
 	const dateBuilder = (d) => {
@@ -15,6 +18,50 @@ function Wheather() {
 	
 		return `${day} ${date} ${month} ${year}`
 	  }
+	const IconSelector = (id) =>
+	{
+		const iconCodes = {
+			2: Icons.storm,
+			3: Icons.rainy,
+			5: Icons.rainy,
+			6: Icons.snow,
+			7: Icons.mist,
+			800: Icons.sunny,
+			801: Icons.cloudy
+		}
+		if(Math.round(id/100)==8)
+		{
+			
+			if(id===801)
+			{
+				return iconCodes[801]
+			}
+			else if(id===800)
+			{
+				return iconCodes[800]
+			}
+			else{
+				return iconCodes[801]
+			}
+		}
+		switch(Math.round(id/100))
+		{
+			case 2:
+				return iconCodes[2]
+			case 3:
+				return iconCodes[3]
+			case 5:
+				return iconCodes[5]
+			case 6:
+				return iconCodes[6]
+			case 7:
+				return iconCodes[7]
+			default:
+				return iconCodes[800]
+		}
+
+	}
+    
 	return (
 		<div className="wheater-box wbox" style={{padding: '1rem'}}>
 			<Typography variant="h5" component="div"> 
@@ -30,10 +77,18 @@ function Wheather() {
 			{wdata.wdata ? Math.round(wdata.wdata.main.temp)  :""} 
 			{wdata.wdata ? <span>&#8451;</span>:""}
 			</Typography>
-			<Typography variant="h4" gutterBottom component="div" sx={{mt: 5}}> 
+					
+			
+			{wdata.wdata ? <div className="wicon-container" style={{position: 'relative',width: '32px',height: '32px',margin: 'auto'}}>
+
+				<img src={IconSelector(wdata.wdata.weather[0].id)} alt="weather-img" style={{position: 'absolute',left:0,width:'100%',height: '100%',objectFit:'cover'}}/>
+			</div>: ""}
+			<Typography variant="h4" gutterBottom component="div" sx={{mt: 5}} sx={{mt: '10px !important'}}> 
 			
 			{wdata.wdata ? wdata.wdata.weather[0].main:""}	
 			</Typography>
+			{wdata.wdata ? console.log(wdata.wdata.weather[0].id) : ""}
+			
 		</div>
 	)
 }
---
+++ src/images.js
@@ -0,0 +1,9 @@
+const Icons = {
+	sunny: require('./imgs/icons/sunny.png').default,
+	mist: require('./imgs/icons/mist.png').default,
+	rainy: require('./imgs/icons/rainy.png').default,
+	snow: require('./imgs/icons/snow.png').default,
+	storm: require('./imgs/icons/storm.png').default,
+	cloudy: require('./imgs/icons/cloudy.png').default,
+}
+export default Icons
